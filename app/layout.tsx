bad3888eece62371ae2ad5608f8694d5ba9f4bf8
Jash <v.jashwanth.2005@gmail.com>
2025-08-24 11:12:23 +0530

    chore: Clean up layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,26 +27,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* Google Tag Manager */}
-      <Script id="gtm-init" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WZMZ936K');`}
-      </Script>
-      {/* End Google Tag Manager */}
+  {/* Google Analytics 4 */}
+  <Script src="https://www.googletagmanager.com/gtag/js?id=G-Y3TDH790Z7" strategy="afterInteractive" />
+  <Script id="ga4-init" strategy="afterInteractive">
+    {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+gtag('config', 'G-Y3TDH790Z7', { anonymize_ip: true });`}
+  </Script>
+  {/* End Google Analytics 4 */}
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WZMZ936K"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <Providers>
             {children}
